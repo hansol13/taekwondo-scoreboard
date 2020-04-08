0123554d3191d6e177a7589bb9ad9a4c3c6cece2
hansol13 <baekmichael96@gmail.com>
2020-04-08 12:47:50 -0400

    refresh button pushed right | restructured code to make it into 2 sections | added deductions counters

--- main/poomsae/poomsaeScoring.js
+++ main/poomsae/poomsaeScoring.js
@@ -1,6 +1,10 @@
 let accuracyScore = 4.0;
 let presentationScore = 6.0;
 let totalScore = 10.0;
+let accuracyMajorDeductionCount = 0;
+let accuracyMinorDeductionCount = 0;
+let presentationMajorDeductionCount = 0;
+let presentationMinorDeductionCount = 0;
 
 function totalScoreUpdate() {
     let totalScore = accuracyScore + presentationScore;
@@ -9,17 +13,41 @@ function totalScoreUpdate() {
 
 
 function accuracyDeduction(major) {
+    if(accuracyScore<0.1) return;
     let deduction = major ? 0.3 : 0.1;
     accuracyScore = accuracyScore - deduction;
-    if(accuracyScore<=0) accuracyScore = 0;
+    if(accuracyScore<=0) accuracyScore=0;
+
+    // handle deduction counters
+    if(major){
+        accuracyMajorDeductionCount++;
+        document.getElementById("accuracyMajorDeductionCount").innerHTML = accuracyMajorDeductionCount;
+    } else{
+        accuracyMinorDeductionCount++;
+        document.getElementById("accuracyMinorDeductionCount").innerHTML = accuracyMinorDeductionCount;
+    }
+
+    // update scores
     document.getElementById("accuracyScore").innerHTML = accuracyScore.toFixed(1);
     totalScoreUpdate();
 }
 
 function presentationDeduction(major) {
+    if(presentationScore<0.1) return;
     let deduction = major ? 0.3 : 0.1;
     presentationScore = presentationScore - deduction;
     if(presentationScore<=0) presentationScore = 0;
+    
+    // handle deduction counters
+    if(major){
+        presentationMajorDeductionCount++;
+        document.getElementById("presentationMajorDeductionCount").innerHTML = presentationMajorDeductionCount;
+    } else{
+        presentationMinorDeductionCount++;
+        document.getElementById("presentationMinorDeductionCount").innerHTML = presentationMinorDeductionCount;
+    }
+
+    // update scores
     document.getElementById("presentationScore").innerHTML = presentationScore.toFixed(1);
     totalScoreUpdate();
 }
@@ -30,4 +58,8 @@ function refreshScores(){
     document.getElementById("accuracyScore").innerHTML = accuracyScore.toFixed(1);
     document.getElementById("presentationScore").innerHTML = presentationScore.toFixed(1);
     document.getElementById("totalScore").innerHTML = totalScore.toFixed(1);
+}
+
+function deductionStatisticUpdate(major, category) {
+    
 }
